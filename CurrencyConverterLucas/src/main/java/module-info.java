79d90ea0e2f0533ac7d41cce63e61eedddf2a310
LucasCoutinho200563162 <134244960+LucasCoutinho200563162@users.noreply.github.com>
2024-04-05 17:22:41 -0400

    Delete CurrencyConverterLucas directory

--- CurrencyConverterLucas/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.currencyconverterlucas {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-
-    opens org.example.currencyconverterlucas to javafx.fxml;
-    exports org.example.currencyconverterlucas;
-}
